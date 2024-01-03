dfa4ebd66ad33ba6d83dd5b485723d3366b88b38
khang2000 <khanghtqt70@gmail.com>
2024-01-03 08:56:28 +0700

    update main

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ let arr = [10, 20, 30, 40, 50];
 let position = arr.indexOf(30);
 console.log(position); // Kết quả: 2
 
-let str1 = "Xin chào, ";
-let str2 = "đây là ";
-let str3 = "một ví dụ ";
-let str4 = "về concat() trong JavaScript.";
+let strA = "apple";
+let strB = "banana";
 
-let combinedString = str1.concat(str2, str3, str4);
-console.log(combinedString);
-// Kết quả là: "Xin chào, đây là một ví dụ về concat() trong JavaScript."
+let comparison = strA.localeCompare(strB);
+if (comparison === 0) {
+  console.log("Chuỗi bằng nhau.");
+} else if (comparison < 0) {
+  console.log("Chuỗi A đứng trước chuỗi B.");
+} else {
+  console.log("Chuỗi B đứng trước chuỗi A.");
+}
