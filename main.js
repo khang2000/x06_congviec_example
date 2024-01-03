930f2c63ccb7d33bc95dc2b9b48f4696dc0a0480
khang2000 <khanghtqt70@gmail.com>
2024-01-03 08:39:45 +0700

    update

---
+++ main.js
@@ -0,0 +1,8 @@
+let str1 = "Xin chào, ";
+let str2 = "đây là ";
+let str3 = "một ví dụ ";
+let str4 = "về concat() trong JavaScript.";
+
+let combinedString = str1.concat(str2, str3, str4);
+console.log(combinedString);
+// Kết quả là: "Xin chào, đây là một ví dụ về concat() trong JavaScript."
